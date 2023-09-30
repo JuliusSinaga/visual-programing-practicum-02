9ef71a1ab0645276db64b98fe9c9501eec517576
Julius Sinaga <143679205+JuliusSinaga@users.noreply.github.com>
2023-09-30 23:13:30 +0700

    Tugas Tutor Selesai

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,42 @@
-// NIM - Your Name
+// 12S23035 - Julius Kaisar Bona Theo Sinaga
+
+import java.util.*;
+import java.lang.Math;
+
+class P02_01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String tugas, kodematkul, matkul, dosenpengampu, deadline, nimnamamatkul, status, rekomendasitugas;
+        int tingkatkesulitan, harihinggadeadline;
+        double prioritas;
+
+        tugas = input.nextLine();
+        kodematkul = input.nextLine();
+        matkul = input.nextLine();
+        dosenpengampu = input.nextLine();
+        deadline = input.nextLine();
+        nimnamamatkul = input.nextLine();
+        tingkatkesulitan = input.nextInt();
+        harihinggadeadline = input.nextInt();
+        status = input.nextLine();
+        prioritas = tingkatkesulitan * (1.0 / harihinggadeadline);
+        if (prioritas > 3.0) {
+            rekomendasitugas = "Penting!Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas < 3.0 && prioritas > 1.5) {
+                rekomendasitugas = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    rekomendasitugas = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println("Prioritas : " + toFixed(prioritas,2));
+        System.out.println(tugas + "|" + kodematkul + "|" + matkul + "|" + dosenpengampu + "|" + deadline + "|" + nimnamamatkul + "|" + status + "|" + rekomendasitugas);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,34 @@
-//NIM -Nama
+// 12S23035 - Julius Kaisar Bona Theo Sinaga
+
+import java.util.*;
+import java.lang.Math;
+
+class P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namagudang, aC, lantai, ketersediaanteknologi, kualitasgudang;
+        int kapasitasgudang, jumlahbuku, skorgudang;
+
+        namagudang = input.nextLine();
+        kapasitasgudang = input.nextInt();
+        jumlahbuku = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanteknologi = input.nextLine();
+        skorgudang = (int) ((double) jumlahbuku / kapasitasgudang * 100);
+        if (skorgudang >= 40) {
+            kualitasgudang = "Gudang Elite";
+        } else {
+            if (skorgudang >= 25) {
+                kualitasgudang = "Gudang Standar";
+            } else {
+                if (skorgudang < 25) {
+                    kualitasgudang = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println("Informasi Gudang: ");
+        System.out.println(namagudang + "|" + kapasitasgudang + "|" + jumlahbuku + "|" + aC + "|" + lantai + "|" + ketersediaanteknologi + "|" + skorgudang + "|" + kualitasgudang);
+    }
+}
